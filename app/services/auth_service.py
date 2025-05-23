4c622f82ff97199a5f43cae519934ac670f3c239
Lucas S <lucasluke307@gmail.com>
2025-05-22 22:47:01 -0300

    feat(auth): implement AuthService, JWTService, and TokenSchema for authentication

---
+++ app/services/auth_service.py
@@ -0,0 +1,55 @@
+from fastapi import HTTPException, status
+from pytest import Session
+from app.models.client_model import ClientModel
+from app.schemas.token_schema import TokenSchema
+from app.schemas.user_schema import UserCreate
+from app.services.jwt_service import JWTService
+from app.services.user_service import UserService
+import jwt
+
+INVALID_CREDENTIALS = "Invalid email or password"
+INVALID_REFRESH_TOKEN = "Invalid refresh token"
+REFRESH_TOKEN_EXPIRED = "Refresh token expired"
+UNAUTHORIZED = status.HTTP_401_UNAUTHORIZED
+
+class AuthService:
+    def __init__(self, user_service: UserService, jwt_service: JWTService):
+        self.user_service = user_service
+        self.jwt_service = jwt_service
+
+    def register(self, db: Session, data: UserCreate) -> ClientModel:
+        return self.user_service.create_user(db, data)
+
+    def login(self, db:Session, email: str, password: str) -> TokenSchema:
+        user = self.user_service.get_user_by_email(db, email)
+        if not user or not self.user_service.verify_password(password, user.password):
+            raise HTTPException(
+                status_code=UNAUTHORIZED,
+                detail=INVALID_CREDENTIALS
+            )
+
+        token_data = {"sub": user.email}
+        access_token = self.jwt_service.create_access_token(data=token_data)
+
+        return TokenSchema(access_token=access_token)
+
+    def refresh_token(self, refresh_token: str) -> str:
+        try:
+            payload = self.jwt_service.decode_token(refresh_token)
+            email = payload.get("sub")
+            if not email:
+                raise HTTPException(
+                    status_code=UNAUTHORIZED,
+                    detail=INVALID_REFRESH_TOKEN
+                )
+            return self.jwt_service.create_access_token({"sub": email})
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(
+                status_code=UNAUTHORIZED,
+                detail=REFRESH_TOKEN_EXPIRED
+            )
+        except jwt.InvalidTokenError:
+            raise HTTPException(
+                status_code=UNAUTHORIZED,
+                detail=INVALID_REFRESH_TOKEN
+            )
