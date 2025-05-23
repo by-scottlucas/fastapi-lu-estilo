7765ddce9c83d487baa117f7c6bbe8e779c6c535
Lucas S <lucasluke307@gmail.com>
2025-05-22 21:23:14 -0300

    refactor(db): refactor ClientModel and generate initial migration

--- app/models/client_model.py
+++ app/models/client_model.py
@@ -5,7 +5,7 @@ class ClientModel(Base):
     __tablename__ = "tb_clients"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True, nullable=False)
-    cpf = Column(String, unique=True, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
-    password = Column(String, nullable=False)
+    name = Column(String(120), unique=True, index=True, nullable=False)
+    cpf = Column(String(14), unique=True, index=True, nullable=False)
+    email = Column(String(120), unique=True, index=True, nullable=False)
+    password = Column(String(128), nullable=False)
